c07678fe3e640bbf7121b5e0c4570a4cd0655921
vedant-kalbag-wkg <vedant.kalbag@weknowgroup.com>
2026-05-09 17:13:57 +0530

    feat(09-03): register weeklyPocAlertsFn in Inngest serve handler

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,8 +2,9 @@ import { serve } from "inngest/next";
 
 import { inngest } from "@/inngest/client";
 import { sendEmailFn } from "@/inngest/functions/send-email";
+import { weeklyPocAlertsFn } from "@/inngest/functions/weekly-poc-alerts";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [sendEmailFn],
+  functions: [sendEmailFn, weeklyPocAlertsFn],
 });
